f09734d99ec9e802f14aad7e5648f9cc790a9d2f
Trayan Iliev <office@iproduct.org>
2019-05-20 14:23:41 +0300

    IOException wrapper added

--- invoicing/src/invoicing/dao/BinaryFileProductRepositoryImpl.java
+++ invoicing/src/invoicing/dao/BinaryFileProductRepositoryImpl.java
@@ -1,75 +1,89 @@
 package invoicing.dao;
 
+import java.io.IOException;
 import java.util.Collection;
 import java.util.Optional;
 
-import invoicing.dao.infrastructure.RepositoryImpl;
 import invoicing.exceptions.EntityDoesNotExistException;
 import invoicing.exceptions.EntityExistsException;
+import invoicing.exceptions.FileIOException;
+import invoicing.model.Measure;
 import invoicing.model.Product;
 
-public class BinaryFileProductRepositoryImpl extends ProductRepositoryImpl 
-		implements ProductRepository {
+public class BinaryFileProductRepositoryImpl extends ProductRepositoryImpl implements ProductRepository {
 
 	public BinaryFileProductRepositoryImpl() {
 	}
 
 	@Override
 	public Optional<Product> findById(String id) {
-		if(entries.size() > 0) {
-			loadProducts();
+		if (entries.size() > 0) {
+			try {
+				loadProducts();
+			} catch (IOException e) {
+				throw new FileIOException(e);
+			}
 		}
 		return super.findById(id);
 	}
 
-
-
 	@Override
 	public Collection<Product> findAll() {
-		if(entries.size() > 0) {
-			loadProducts();
+		if (entries.size() > 0) {
+			try {
+				loadProducts();
+			} catch (IOException e) {
+				throw new FileIOException(e);
+			}
 		}
 		return super.findAll();
 	}
 
-
-
 	@Override
 	public Product add(Product entity) throws EntityExistsException {
 		Product result = super.add(entity);
-		saveProducts();
+		try {
+			saveProducts();
+		} catch (IOException e) {
+			throw new FileIOException(e);
+		}
 		return result;
 	}
-
-
 
 	@Override
 	public Product update(Product entity) throws EntityDoesNotExistException {
 		Product result = super.update(entity);
-		saveProducts();
+		try {
+			saveProducts();
+		} catch (IOException e) {
+			throw new FileIOException(e);
+		}
 		return result;
 	}
-
-
 
 	@Override
 	public Optional<Product> delete(String id) {
 		Optional<Product> result = super.delete(id);
-		saveProducts();
+		try {
+			saveProducts();
+		} catch (IOException e) {
+			throw new FileIOException(e);
+		}
 		return result;
 	}
 
-	private void loadProducts() {
-		
-		
-	}
-	
-	private void saveProducts() {
-		
-		
+	private void loadProducts() throws IOException {
+		for(Product p : entries.values()) {
+			p.getMeasure().ordinal();
+		}
+			
 	}
 
-
+	private void saveProducts() throws IOException {
+		Product p = new Product();
+		int ordinal = 1; // read from file
+		p.setMeasure(Measure.values()[ordinal]);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
---
+++ invoicing/src/invoicing/exceptions/FileIOException.java
@@ -0,0 +1,22 @@
+package invoicing.exceptions;
+
+public class FileIOException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public FileIOException() {
+	}
+
+	public FileIOException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+	}
+
+	public FileIOException(String arg0) {
+		super(arg0);
+	}
+
+	public FileIOException(Throwable arg0) {
+		super(arg0);
+	}
+	
+}
