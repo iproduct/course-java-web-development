a50a8af0cac41284fc2f36612ba984784557e9a7
Trayan Iliev <office@iproduct.org>
2019-05-23 15:47:39 +0300

    tests added to project and Ant build.xml

---
+++ InvoicingWithViews/test/invoicing/controller/InvoiceRegisterTest.java
@@ -0,0 +1,19 @@
+package invoicing.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class InvoiceRegisterTest {
+
+	@Test
+	public void testFindProductByProductCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddProduct() {
+		fail("Not yet implemented");
+	}
+
+}
