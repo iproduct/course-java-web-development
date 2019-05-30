24ec30e0f8f5354b6a2320079a33159a989e9919
Trayan Iliev <office@iproduct.org>
2019-05-30 15:24:42 +0300

    react experiment

---
+++ 09-myblog-js-client/src/test.js
@@ -0,0 +1,21 @@
+const a = [1,2,3,4,5,6,7,8];
+
+let [a1, a2, ...rest] = a;
+
+// console.log(`${a1}| ${a2}| ${rest}`);
+
+let persons = [
+    { name: 'Michael Harrison',
+      parents: {
+        mother: 'Melinda Harrison',
+        father: 'Simon Harrison',
+      }, age: 35},
+    { name: 'Robert Moore',
+      parents: {
+        mother: 'Sheila Moore',
+        father: 'John Moore',
+      }, age: 25}];
+  for (let {name: n, parents: { father: f }, age } of persons) {
+    console.log(`Name: ${n}, Father: ${f}, age: ${age}`);
+  }
+
