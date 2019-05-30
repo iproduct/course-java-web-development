77ff40fe7da521f77bd78380c547c4c9c4cccfc5
Trayan Iliev <office@iproduct.org>
2019-05-30 15:24:09 +0300

    react experiment

--- 09-spring-boot-demo/src/main/resources/static/app/index.js
+++ 09-spring-boot-demo/src/main/resources/static/app/index.js
@@ -1,29 +1,28 @@
-//const myelement = (<p>Hello, world! I am new  CommentBox.</p>);
+const myelement = (<p>Hello, world! I am new  CommentBox.</p>);
+const myelement2 = React.createElement('p',{},'Hello, world! I am new  CommentBox 2.' );
 
-class App extends React.Component {
-	state = {
-		articles:[]
-	};
-	
-	constructor(){
-	super();
-	fetch('http://localhost:8080/api/articles')
-    	.then(resp => resp.json())
-    	.then(articles => {
-    		console.log(articles);
-			this.setState({articles});
-    	});
-	}
-	
-	render() {
-	    return this.state.articles.map(
-		  article => (<Article key={article.id} value={article} />)
-	    );
-	}
-
-}
-
-const Article = ({value}) => (<div>{value.id} -> {value.title} : {value.content}</div>);
-
-
-ReactDOM.render(<App/>, document.getElementById('root'));
+//class App extends React.Component {
+//	state = {
+//		articles:[]
+//	};
+//	
+//	constructor(){
+//	super();
+//	fetch('http://localhost:8080/api/articles')
+//    	.then(resp => resp.json())
+//    	.then(articles => {
+//    		console.log(articles);
+//			this.setState({articles});
+//    	});
+//	}
+//	
+//	render() {
+//	    return this.state.articles.map(
+//		  article => (<Article key={article.id} value={article} />)
+//	    );
+//	}
+//
+//}
+//
+//const Article = ({value}) => (<div>{value.id} -> {value.title} : {value.content}</div>);
+ReactDOM.render(myelement2, document.getElementById('root'));
