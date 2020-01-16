248ad768dfed75ae96e519c32dafc6cc26f30483
Trayan Iliev <office@iproduct.org>
2020-01-16 14:29:55 +0200

    ChatHandler Callable added

---
+++ 01-java-net-lab2/src/chatserver/ChatHandler.java
@@ -0,0 +1,61 @@
+package chatserver;
+
+import static java.util.logging.Level.SEVERE;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.Socket;
+import java.util.concurrent.Callable;
+import java.util.logging.Logger;
+
+import timeserver.TimeServer;
+
+public class ChatHandler implements Callable<Void> {
+	public static final String SOCKET_ENCODING = "utf-8";
+	public static Logger logger = Logger.getLogger(ChatHandler.class.getSimpleName());
+
+	private ChatServer server;
+	private final Socket socket;
+	private BufferedReader in;
+	private PrintWriter out;
+	private String nickname;
+
+	public ChatHandler(ChatServer server, Socket socket) {
+		this.server = server;
+		this.socket = socket;
+		try {
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream(), SOCKET_ENCODING));
+			PrintWriter out = new PrintWriter(
+					new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), SOCKET_ENCODING)));
+		} catch (UnsupportedEncodingException e) {
+			logger.log(SEVERE, "Can not recognize encoding: " + SOCKET_ENCODING, e);
+		} catch (IOException e) {
+			logger.log(SEVERE, "Can not open input/output stream for socket: " + socket, e);
+		}
+
+	}
+
+	@Override
+	public Void call() throws Exception {
+		String message = "";
+		nickname = in.readLine();
+		while(!"end".equalsIgnoreCase(message)) {
+			message = in.readLine();
+			server.sendToAll(nickname + ": " + message);
+		}
+		logger.info("User '" + nickname + "' logged out.");
+		socket.close();
+
+		return null;
+	}
+	
+	public void sendMessage(String message) {
+		out.println(message);
+	}
+
+}
---
+++ 01-java-net-lab2/src/chatserver/ChatServer.java
@@ -0,0 +1,15 @@
+package chatserver;
+
+public class ChatServer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void sendToAll(String message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
