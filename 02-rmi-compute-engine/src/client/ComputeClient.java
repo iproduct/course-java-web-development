55bcd44c86f95429c8b3e1c2c6c0025872932a95
Trayan Iliev <office@iproduct.org>
2020-01-20 07:59:25 +0200

    RMI demo added

---
+++ 02-rmi-compute-engine/src/client/ComputeClient.java
@@ -0,0 +1,33 @@
+package client;
+
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+import contract.Compute;
+
+import java.math.BigDecimal;
+
+public class ComputeClient {
+
+	public static void main(String args[]) {
+		if (System.getSecurityManager() == null) {
+			System.setSecurityManager(new SecurityManager());
+		}
+		try {
+			String name = "ComputationServr";
+			Registry registry = LocateRegistry.getRegistry(args[0], Integer.parseInt(args[1]));
+			Compute comp = (Compute) registry.lookup(name);
+			ComputePiTask task = new ComputePiTask(Integer.parseInt(args[2]));
+			BigDecimal pi = comp.executeTask(task);
+			System.out.println(pi);
+		} catch (RemoteException e) {
+			System.err.println("ComputePi exception:");
+			e.printStackTrace();
+		} catch (NotBoundException e) {
+			System.err.println("Server not registered with RMI Registry.");
+			e.printStackTrace();
+		}
+	}
+}
