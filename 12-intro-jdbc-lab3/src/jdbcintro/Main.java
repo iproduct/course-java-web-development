17104f3148a3a69a27f0343df803febd569015c9
Trayan Iliev <office@iproduct.org>
2020-03-05 12:12:31 +0200

    basic jdbc demo completed

--- 12-intro-jdbc-lab3/src/jdbcintro/Main.java
+++ 12-intro-jdbc-lab3/src/jdbcintro/Main.java
@@ -2,6 +2,8 @@ package jdbcintro;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Properties;
 import java.util.Scanner;
@@ -36,6 +38,29 @@ public class Main {
 		Connection con = DriverManager.getConnection(
 				"jdbc:mysql://localhost:3306/organization?createDatabaseIfNotExist=true", props);
 		System.out.println("Connected successfully.");
+		
+		// 3. Execute statement
+		System.out.println("Enter minimal salary (20000 for default):");
+		String salaryStr  = sc.nextLine().trim();
+		salaryStr  = salaryStr.length() > 0 ? salaryStr : "20000";
+		
+		double salary = Double.parseDouble(salaryStr);
+		
+		PreparedStatement s = con.prepareStatement("SELECT * FROM employees WHERE salary > ?");
+		s.setDouble(1, salary);
+		
+		ResultSet rs = s.executeQuery();
+		
+		// 4. Process results
+		int n = 0;
+		while(rs.next()) {
+			
+			System.out.printf("| %4d | %-15.15s | %-15.15s | %10.2f |%n",
+					++n, rs.getString(2), rs.getString("last_name"), rs.getDouble("salary"));					
+		}
+		
+		// 5. Close connection
+		con.close();
 
 	}
 
