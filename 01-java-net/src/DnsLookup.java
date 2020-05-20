fd56dafbe33ac1cbd25a92f260c4882af22236bd
Trayan Iliev <office@iproduct.org>
2020-05-20 07:02:40 +0300

    dns lookup url changed

--- 01-java-net/src/DnsLookup.java
+++ 01-java-net/src/DnsLookup.java
@@ -1,17 +1,12 @@
-//: c15:WhoAmI.java
-// From 'Thinking in Java, 2nd ed.' by Bruce Eckel
-// www.BruceEckel.com. See copyright notice in CopyRight.txt.
-// Finds out your network address when
-// you're connected to the Internet.
 import java.net.*;
 
 public class DnsLookup {
   public static void main(String[] args) throws Exception {
 
-    InetAddress[] addr = InetAddress.getAllByName("google.com");
+    InetAddress[] addr = InetAddress.getAllByName("yahoo.com");
     for(InetAddress a : addr) {
 	    String domain  = a.getCanonicalHostName();
 	    System.out.printf("%s --> %s [Accessible: %s]\n", a, domain, a.isReachable(2000));
     }
   }
-} ///:~
+}
